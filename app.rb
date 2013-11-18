c8133ec62fe07a5150931ff14bfa82c9442e5f7e
Steve Kinney <hello@stevekinney.net>
2013-11-18 11:41:44 -0500

    Convert into a webapp

--- app.rb
+++ app.rb
@@ -6,30 +6,38 @@ require 'haml'
 require './lib/vertical-response'
 require './lib/blue-state-headers'
 
+encoding_options = {
+      :invalid => :replace,
+      :undef => :replace,
+      :replace => '?'
+    }
+
 get "/" do
   haml :upload
 end
 
 post "/" do
-  input = params[:file][:tempfile].read
-  input = input.encode('utf-8')
-  # input = input.gsub(/\r\n?/, '\n')
-  CSV.read(input, headers: :first_row).each do |row|
-    output << VerticalResponse.new(row).to_row
+  content_type 'text/csv'
+  input = params[:file][:tempfile].read.encode('UTF-8', encoding_options)
+  output = Output.new
+  CSV.parse(input, headers: :first_row).each do |row|
+    output << VerticalResponse.new(row).to_s
   end
   return output.to_s
 end
 
-
-# Dir.glob('./vertical-response/*.csv').each do |file|
-#   output_file = File.new("./blue-state/#{file[20,file.length - 4]}-#{Time.new.strftime('%F-%3N%S')}.csv", 'w')
-#   output = CSV.new(output_file)
-#   output << HEADERS
-#   CSV.read(file, headers: :first_row).each do |row|
-#     output << VerticalResponse.new(row).to_row
-#   end
-# end
-
-# File.open(params['file'][:filename], "w") do |f|
-#   f.write(params['file'][:tempfile].read)
-# end
+class Output
+  
+  def initialize
+    @data = []
+  end
+  
+  def <<(row)
+    @data << row
+  end
+  
+  def to_s
+    @data.join("\n")
+  end
+  
+end
